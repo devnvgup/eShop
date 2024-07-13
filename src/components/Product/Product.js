0d06d51317fac06cfa7525f70be51bb68056e531
Nguyen Quoc Truong <nguyenquoctruong@Truong-Dev.local>
2024-07-13 09:58:04 +0700

    first commit

---
+++ src/components/Product/Product.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Product.css"
+
+function Product() {
+    return (
+        <div className='product'>
+            <div className='product__info'>
+                <p>Bennett Mystic 15.6 inch Laptop Shoulder Messenger Sling Office Bag, Water Repellent Fabric for Men and Women (Blue)"
+                </p>
+                <p className='product__price'>
+                    <small>$</small>
+                    <strong>30</strong>
+                </p>
+                <div className='product__rating'>⭐️</div>
+            </div>
+            <img className='product__img' src='https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg' alt=''/>
+
+                <button className='product__btn'>Add to Basket</button>
+        </div>
+    )
+}
+
+export default Product
